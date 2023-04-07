1586aaeeed3818e0f3639e05d628b8ccd66f5200
kashi <kashi@KASHIFULLAHJAN>
2023-04-07 14:20:17 +0500

    initial commit

---
+++ Game/Unreal/Source/ClaasRoom/Public/ClaasRoomGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClaasRoomGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AClaasRoomGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AClaasRoomGameMode();
+};
+
+
+
